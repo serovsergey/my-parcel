310ca7adfef5cea09b574d679b5dfa785b24a7b5
Serov Sergey <serovsergey79@gmail.com>
2022-08-07 22:41:10 +0300

    init

---
+++ index.js
@@ -0,0 +1,16 @@
+const $4fa36e821943b400$var$b = 5 ?? 4;
+const $4fa36e821943b400$var$m = new Map();
+console.log($4fa36e821943b400$var$m);
+const $4fa36e821943b400$var$f = ()=>{
+    console.log("first");
+};
+class $4fa36e821943b400$var$A {
+    constructor(name){
+        this.name = name;
+    }
+}
+const $4fa36e821943b400$var$a = new $4fa36e821943b400$var$A("test");
+console.log($4fa36e821943b400$var$a);
+
+
+//# sourceMappingURL=index.js.map
---
+++ src/index.js
@@ -0,0 +1,15 @@
+const b = 5 ?? 4;
+const m = new Map();
+console.log(m)
+
+const f = () => {
+    console.log('first')
+}
+class A {
+    constructor(name) {
+        this.name = name
+    }
+}
+
+const a = new A('test');
+console.log(a);
